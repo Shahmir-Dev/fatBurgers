1d94bf4ee44b733d37ab8f6eff661293e0e4ed21
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-10-09 21:40:39 -0400

    updated buttons and functionality

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,36 +1,59 @@
-//Create new burger 
-$(".submit").on("click", function (event) {
+(function () {
+    $(".change-devour").on("click", function (event) {
+        console.log("devour button change")
+        var id = $(this).data("id");
 
-    event.preventDefault();
+        var newDevourState = {
+            devoured: true
+        };
 
-    const burger_name = {
-        burger_name: $("#burger").val().trim(),
-    };
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                console.log("changed devoured to", newDevourState);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 
-    $.ajax("/burger", {
-        type: "POST",
-        data: burger_name
-    }).then(
-        function () {
-            console.log("created new burger");
-            location.reload();
-        }
-    );
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        console.log("burger button add")
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#ca").val().trim(),
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                location.reload();
+                // Reload the page to get the updated list
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 });
-
-//Devour a Burger 
-$('.eatMe').click(function () {
-    let id = $(this).data('id');
-
-    let devourState = {
-        devoured: true
-    }
-
-    $.ajax(`/burgers/${id}`, {
-        method: 'PUT',
-        data: devourState
-    }).then(function (data) {
-        console.log("NOM NOM NOM")
-        location.reload();
-    })
-})
--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 // Set Handlebars.
 const exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
